6dae328ab21ea09abfa44bc39b73dcd2f72a8220
Ashwin sawkare <asawakare@gammastack.com>
2022-01-24 15:54:00 +0530

    added prefrence option

--- game_app/views.py
+++ game_app/views.py
@@ -11,6 +11,8 @@ def home(request):
         username = request.POST.get('username')
         option = request.POST.get('option')
         room_code = request.POST.get('room_code')
+        prefrence = request.POST.get('prefrence')
+        print(prefrence)
         if option == '1':
             game = Game.objects.filter(room_code=room_code).first()
             if game is None:
@@ -24,23 +26,23 @@ def home(request):
             game.game_opponent = username
             game.save()
             print(room_code, username)
-            return redirect('/play/' + room_code + '?username='+username)
+            return redirect('/play/' + room_code + '?prefrence='+prefrence)
             # return redirect('/play/' + room_code + '?username='+username)
         else:
-            game = Game(game_creator=username, room_code=room_code)
+            game = Game(game_creator=username, room_code=room_code, prefrence=prefrence)
             game.save()
             room = Room(room=room_code)
             room.save()
             print(room_code, username)
-            return redirect('/play/' + room_code + '?username='+username)
+            return redirect('/play/' + room_code + '?prefrence='+prefrence)
     return render(request, "index.html")
 
 
 def playgame(request, room_code):
-    username = request.GET.get('username')
-    print(f"username is............{username}")
+    prefrence = request.GET.get('prefrence')
+    print(f"username is............{prefrence}")
     context = {
         "room_code": room_code,
-        'username': username
+        'username': prefrence
         }
     return render(request, "play.html", context)
