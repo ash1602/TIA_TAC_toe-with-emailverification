a2859f79ff2410603765261f509e2d6dd346d2f4
Ashwin sawkare <asawakare@gammastack.com>
2022-03-01 20:46:21 +0530

    complete game with email verifiation

--- tic_tac/settings.py
+++ tic_tac/settings.py
@@ -130,8 +130,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ashwinsawkare02091999@gmail.com'
-EMAIL_HOST_PASSWORD = 'akgqwkumjxrcfrmq'
+EMAIL_HOST_USER = 'youremail@gmail.com'
+EMAIL_HOST_PASSWORD = ''
 
 CHANNEL_LAYERS = {
     'default': {
